6f294b73948317007ae3f0c5b756dcc8feaeb0eb
Panasun Sunanta <panasunsunanta@gmail.com>
2021-10-14 16:04:27 +0700

    feat: add style

--- src/anyrare_web/events.cljs
+++ src/anyrare_web/events.cljs
@@ -1,8 +1,7 @@
 (ns anyrare-web.events
   (:require
    [re-frame.core :as re-frame]
-   [anyrare-web.db :as db]
-   ))
+   [anyrare-web.db :as db]))
 
 (re-frame/reg-event-db
  ::initialize-db
--- src/anyrare_web/views.cljs
+++ src/anyrare_web/views.cljs
@@ -1,6 +1,7 @@
 (ns anyrare-web.views
   (:require
    [re-frame.core :as re-frame]
+   [garden.core :refer [css]]
    [anyrare-web.subs :as subs]
    [anyrare-web.events :as events]))
 
@@ -8,7 +9,8 @@
   (let [name (re-frame/subscribe [::subs/name])
         test (re-frame/subscribe [::subs/test])]
     [:div
-     [:p "Test value: " @test]
+     [:p {:style {:font-size "32px" :color "#930293" :text-decoration "underline"}} 
+      "Test value: " @test]
      [:p "Name value: " @name]
      [:button {:on-click #(re-frame/dispatch [::events/name-change "Name Changed!"])} "Change name"]
      [:h1
