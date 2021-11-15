95fd1c581e0f26fceb0032e458b147a3b98981a2
Panasun Sunanta <panasunsunanta@gmail.com>
2021-11-15 18:29:17 +0700

    fix: add swipe.js

--- src/app/asset/events.cljs
+++ src/app/asset/events.cljs
@@ -1,4 +1,11 @@
 (ns app.asset.events
   (:require
    [re-frame.core :refer [reg-event-db]]
+   ["swipejs" :as Swipe]
    [app.asset.db :as db]))
+
+(reg-event-db
+ ::add-slider
+ (fn []
+   (-> (Swipe. (.getElementById js/document "slider"))
+       (as-> x (.log js/console x)))))
--- src/app/asset/views.cljs
+++ src/app/asset/views.cljs
@@ -1,94 +1,114 @@
 (ns app.asset.views
   (:require
-   [re-frame.core :refer [subscribe]]
+   [re-frame.core :refer [subscribe dispatch]]
+   [herb.core :refer [<class]]
    [tailwind-hiccup.core :refer [tw]]
    [app.asset.subs :as subs]
    [app.asset.events :as events]
-   ["swiper" :default Swiper]
    [app.config.i18n :refer [i18n]]))
 
-(def swiper (.log js/console "Swiper" (Swiper. ".mySwiper")))
+
+(defn image-carousel [images]
+  (for [image images]
+    [:div 
+     [:img {:src image}]]))
+
+;; (def swipe (Swipe. (.getElementById js/document "slider")))
+
+
+
+;; (set! (.. js/window -mySwipe) swipe)
+
+(defn description [text showFull]
+  [:div (if (true? showFull) text (subs text 0 (min (count text) 100)))])
 
 (defn asset []
-  (let [asset (subscribe [::subs/asset])]
-    [:div (tw [:flex :px-2 :flex-wrap])
-     [:div (tw [:flex :flex-auto])
-      [:div (tw [:mx-auto :lg:max-w-lg :xl:max-full])
-       [:div  {:class "w-full h-full mySwiper"}
-        [:div
-         [:div "Slide1"]
-         [:div "Slide2"]
-         [:div "Slide3"]
-         ]]
-       [:img
-        {:src "https://cf.lnwfile.com/2gwbkl.jpg" :class "rounded-lg object-cover h-full"}]]]
-     [:div (tw [:mt-4 :w-full :lg:max-w-md :xl:max-w-lg])
-      [:div [:h1 (tw [:text-3xl :font-kanit :font-medium]) (get-in @asset [:title :th])]]
-      [:div
-       [:span
-        (tw [:text-secondary :text-sm :font-kanit :font-medium])
-        (i18n :highest-bid)]
-       [:span
-        (tw [:text-sm :font-bold :ml-1 :text-transparent :bg-clip-text :bg-gradient-to-br :from-red-400 :to-purple-800])
-        "1.0391 ARA"]]
-      [:div
-       (tw [:mt-2])
-       "พระนางพญา เนื้อดำ พิมพ์เข่าโค้ง กรุวัดนางพญา พิษณุโลกผ่านพิธีกรรมปลุกเสกจากเกจิอาจารย์ชื่อดัง สุดยอดพุทธคุณที่ควรหามาบูชารุ่นนี้หายากมาก เหมาะกับการสะสมในคอลเลกชั่น..."]
-      [:div
-       (tw [:mt-8 :flex])
-       [:div (tw ["w-1/2"])
-        [:div (tw [:text-sm :font-bold])
-         [:span (tw [:font-bold]) "Founder "]
-         [:span (tw [:text-secondary]) " 10% royalties"]]
-        [:div
-         (tw [:flex :mt-2])
-         [:img {:src "https://cdn.iconscout.com/icon/free/png-256/avatar-373-456325.png" :class "w-12"}]
-         [:span (tw [:font-bold :ml-3 :mt-2]) "BinBangkruai"]]]
-       [:div (tw ["w-1/2"])
-        [:div (tw [:text-sm :font-bold])
-         [:span "Founder"]]
-        [:div
-         (tw [:flex :mt-2])
-         [:img {:src "https://cdn.iconscout.com/icon/free/png-256/avatar-373-456325.png" :class "w-12"}]
-         [:span (tw [:font-bold :ml-3 :mt-2]) "JKBin"]]]]
-      [:div
-       (tw [:mt-8 :font-bold])
-       [:span (tw [:pr-6]) "Details"]
-       [:span (tw [:pr-6 :text-secondary]) "Bids"]
-       [:span (tw [:pr-6 :text-secondary]) "History"]
-       [:hr (tw [:mt-2])]]
-      [:div (tw [:mt-4])
-       [:div (tw [:text-sm :font-bold :text-secondary]) "Custodian"]
-       [:div
-        (tw [:flex :mt-2])
-        [:img {:src "https://cdn.iconscout.com/icon/free/png-256/avatar-373-456325.png" :class "w-12 h-12"}]
-        [:div
-         (tw [:flex :flex-col])
-         [:div (tw [:font-bold :ml-3 :flex-0]) "GPraCustodian"]
-         [:div (tw [:font-bold :ml-3 :text-sm :text-secondary :flex]) "Bangkok, TH"]]]]
-      [:div (tw [:mt-4 :text-sm])
-       [:div (tw [:font-bold :text-secondary]) "Custodian Fees"]
-       [:div
-        (tw [:mt-2])
-        [:p "Storage Fee: 0.102 ARA / Year"]
-        [:p "Due Date: 10/9/2022 2:25 PM"]]
-       [:div
-        (tw [:mt-2])
-        [:p "Insurance Coverage: 1.200 ARA"]
-        [:p "Expired Date: 10/9/2022 2:25 PM"]]]
-      [:div
-       [:div (tw [:mt-4 :text-sm :text-secondary :font-bold]) "Auditor"]
-       [:div (tw [:flex])
-        [:div (tw ["w-1/2"])
-         [:div
-          (tw [:flex :mt-2])
-          [:img {:src "https://cdn.iconscout.com/icon/free/png-256/avatar-373-456325.png" :class "w-12"}]
-          [:span (tw [:font-bold :ml-3 :mt-2]) "JKBin"]]]
-        [:div (tw ["w-1/2" :text-sm])
-         "Audited Date: 10/3/2020 1:37PM"]]]
-      [:div
-       [:div (tw [:mt-4 :text-sm :text-secondary :font-bold]) "Auditor Report"]
-       [:div (tw [:mt-2]) "พระนางพญา เนื้อดำ พิมพ์เข่าโค้ง มีรอยบิ่นขอบบนซ้าย มีจุดตัด ปีพ.ศ.2490 รุ่นเมตตามหานิยม จัดสร้างที่วัดนางพญา จังหวัดพิษณุโลก จำนวน 20,000องค์ ด้านหลังพิมพ์อักขระพิเศษ มีรอยนูนด้านซ้ายล่าง"]]
-      [:div
-       (tw [:mt-4])
-       [:hr]]]]))
+  [:div {:id "slider" :class "swipe mt-16"}
+   [:div {:class "swipe-wrap"}
+    [:div "1"]
+    [:div "2"]
+    [:div "3"]
+    [:div "4"]]]
+  )
+
+;; (defn asset []
+;;   (let [asset (subscribe [::subs/asset])]
+;;     [:div (tw [:flex :px-2 :flex-wrap])
+;;      [:div (tw [:flex :flex-auto])
+;;       [:div (tw [:mx-auto :lg:max-w-lg :xl:max-full])
+;;        [:div {:id "slider" :class "swipe w-full"}]
+;;         [:div {:class ["swipe-wrap w-full"]}]
+;;          [:div {:class "swiper-slide w-64 bg-red-700"} "Slide 1"]
+;;          [:div {:class "swiper-slide w-64 bg-yellow-700"} "Slide 2"]
+;;          [:div {:class "swiper-slide w-64 bg-green-700"} "Slide 3"]]
+;;        [:button {:class [:bg-primary] :on-click #(dispatch [::events/add-slider])} "Add slider"]]
+;;      [:div (tw [:mt-4 :w-full :lg:max-w-md :xl:max-w-lg])
+;;       [:div [:h1 (tw [:text-3xl :font-kanit :font-medium]) (get-in @asset [:title :th])]]
+;;       [:div
+;;        [:span
+;;         (tw [:text-secondary :text-sm :font-kanit :font-medium])
+;;         (i18n :highest-bid)]
+;;        [:span
+;;         (tw [:text-sm :font-bold :ml-1 :text-transparent :bg-clip-text :bg-gradient-to-br :from-red-400 :to-purple-800])
+;;         "1.0391 ARA"]]
+;;       [:div
+;;        (tw [:mt-2])
+;;        (description "พระนางพญา เนื้อดำ พิมพ์เข่าโค้ง กรุวัดนางพญา พิษณุโลกผ่านพิธีกรรมปลุกเสกจากเกจิอาจารย์ชื่อดัง สุดยอดพุทธคุณที่ควรหามาบูชารุ่นนี้หายากมาก เหมาะกับการสะสมในคอลเลกชั่น..." false)]
+;;       [:div
+;;        (tw [:mt-8 :flex])
+;;        [:div (tw ["w-1/2"])
+;;         [:div (tw [:text-sm :font-bold])
+;;          [:span (tw [:font-bold]) "Founder "]
+;;          [:span (tw [:text-secondary]) " 10% royalties"]]
+;;         [:div
+;;          (tw [:flex :mt-2])
+;;          [:img {:src "https://cdn.iconscout.com/icon/free/png-256/avatar-373-456325.png" :class "w-12"}]
+;;          [:span (tw [:font-bold :ml-3 :mt-2]) "BinBangkruai"]]]
+;;        [:div (tw ["w-1/2"])
+;;         [:div (tw [:text-sm :font-bold])
+;;          [:span "Founder"]]
+;;         [:div
+;;          (tw [:flex :mt-2])
+;;          [:img {:src "https://cdn.iconscout.com/icon/free/png-256/avatar-373-456325.png" :class "w-12"}]
+;;          [:span (tw [:font-bold :ml-3 :mt-2]) "JKBin"]]]]
+;;       [:div
+;;        (tw [:mt-8 :font-bold])
+;;        [:span (tw [:pr-6]) "Details"]
+;;        [:span (tw [:pr-6 :text-secondary]) "Bids"]
+;;        [:span (tw [:pr-6 :text-secondary]) "History"]
+;;        [:hr (tw [:mt-2])]]
+;;       [:div (tw [:mt-4])
+;;        [:div (tw [:text-sm :font-bold :text-secondary]) "Custodian"]
+;;        [:div
+;;         (tw [:flex :mt-2])
+;;         [:img {:src "https://cdn.iconscout.com/icon/free/png-256/avatar-373-456325.png" :class "w-12 h-12"}]
+;;         [:div
+;;          (tw [:flex :flex-col])
+;;          [:div (tw [:font-bold :ml-3 :flex-0]) "GPraCustodian"]
+;;          [:div (tw [:font-bold :ml-3 :text-sm :text-secondary :flex]) "Bangkok, TH"]]]]
+;;       [:div (tw [:mt-4 :text-sm])
+;;        [:div (tw [:font-bold :text-secondary]) "Custodian Fees"]
+;;        [:div
+;;         (tw [:mt-2])
+;;         [:p "Storage Fee: 0.102 ARA / Year"]
+;;         [:p "Due Date: 10/9/2022 2:25 PM"]]
+;;        [:div
+;;         (tw [:mt-2])
+;;         [:p "Insurance Coverage: 1.200 ARA"]
+;;         [:p "Expired Date: 10/9/2022 2:25 PM"]]]
+;;       [:div
+;;        [:div (tw [:mt-4 :text-sm :text-secondary :font-bold]) "Auditor"]
+;;        [:div (tw [:flex])
+;;         [:div (tw ["w-1/2"])
+;;          [:div
+;;           (tw [:flex :mt-2])
+;;           [:img {:src "https://cdn.iconscout.com/icon/free/png-256/avatar-373-456325.png" :class "w-12"}]
+;;           [:span (tw [:font-bold :ml-3 :mt-2]) "JKBin"]]]
+;;         [:div (tw ["w-1/2" :text-sm])
+;;          "Audited Date: 10/3/2020 1:37PM"]]]
+;;       [:div
+;;        [:div (tw [:mt-4 :text-sm :text-secondary :font-bold]) "Auditor Report"]
+;;        [:div (tw [:mt-2]) "พระนางพญา เนื้อดำ พิมพ์เข่าโค้ง มีรอยบิ่นขอบบนซ้าย มีจุดตัด ปีพ.ศ.2490 รุ่นเมตตามหานิยม จัดสร้างที่วัดนางพญา จังหวัดพิษณุโลก จำนวน 20,000องค์ ด้านหลังพิมพ์อักขระพิเศษ มีรอยนูนด้านซ้ายล่าง"]]
+;;       [:div
+;;        (tw [:mt-4])
+;;        [:hr]]]]))
