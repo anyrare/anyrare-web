c0ce471b1a5e7cbdaf7c314de03fb851b5fa622a
Panasun Sunanta <panasunsunanta@gmail.com>
2022-01-24 18:06:42 +0700

    feat: connect metamask

--- src/app/home/views.cljs
+++ src/app/home/views.cljs
@@ -47,7 +47,20 @@
        (clj->js {:url "https://testnet.anyrare.network"})
        (clj->js {:chainId 1687 :name "anyrare"})))
 
+(def provider-metamask
+  (new (.-Web3Provider (.-providers ethers)) (.-ethereum js/window)))
+
+(def signer (.getSigner provider-metamask))
+
 (.log js/console provider)
+(.log js/console provider-metamask)
+(-> (.send provider-metamask "eth_requestAccounts" [])
+    (p/then (fn [x]
+              (-> signer
+                  (p/then (fn [_signer]
+                            (-> (.getAddress _signer)
+                                (p/then (fn [x] (.log js/console x))))))
+                  (p/catch* (fn [err] (js/console.error err)))))))
 
 (def member-contract
   (new (.-Contract ethers)
@@ -57,11 +70,42 @@
 
 (.log js/console member-contract)
 
-(-> (.getReferral member-contract "0x1E6C09C1D3Ac114752dbBD3E4e91b0b167ddee84")
+(def user0 "0x1E6C09C1D3Ac114752dbBD3E4e91b0b167ddee84")
+
+(-> (.getReferral member-contract user0)
+    (p/then (fn [x] (js/console.log x)))
+    (p/catch* (fn [err] (js/console.error err))))
+
+(-> (.isMember member-contract user0)
     (p/then (fn [x] (js/console.log x)))
     (p/catch* (fn [err] (js/console.error err))))
 
 (defn home [] [:div "Home"])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
